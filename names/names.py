31e258ab391f13d0bbfac75e8765b40d67143ef0
Doug <me@douglasbowen.com>
2019-09-20 13:58:46 -0400

    add passing reverse.py

--- names/names.py
+++ names/names.py
@@ -20,3 +20,8 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# I'm assuming I need an LRU cache to get this to work, but my LRU cache isn't working so I'm going on to reverse the linked list
+
+# An LRU cache should work because we can set it to 10000 spots and pop a name out whenever they're duplicated. The issue with the runtime (n^2) is because of the nested for loops. So we need a way to not have nested for loops. Some sort of caching would help. As of now, taking each name, and multiplying it by 10000 to check for duplicates, since we have 10000 names in list one, it's 10000*10000 (n^2) for names.
+
+# The LRU Cache might help because we can use a dictionary for the faster lookup and store each node of the linked list in the dictionary, and print the max size of the dictionary. The linked list should contain only duplicated. So we'll be building a linked list of 64 names because there's 64 duplicates. 
--- reverse/reverse.py
+++ reverse/reverse.py
@@ -41,7 +41,39 @@ class LinkedList:
       current = current.get_next()
     # if we've gotten here, then the target node isn't in our list
     return False
-
+# so we need to find the current head, move backwards until it's None, so move backwards until it's pointing at nothing, build a list while it's doing so, and then make a new list using the current numbers in our current list. so something like assuming 3-2-1 is our current list.
+# start at 3, move into temp storage, go to 2, move into temp storage, go to 1, move into temp storage, we see none. start at 3, add 2 to head, add 1 to head
+# we can use recursioni to get to the end of the list
+# iteration will be faster for me right now
   def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+      tempList = []
+      #print(self.head)
+      if self.head is None:
+          return 0
+      while self.head is not None:
+          tempList.append(self.head.value)
+          self.head = self.head.get_next()
+          print(tempList)
+      tempList.reverse()
+      for i in range(len(tempList)):
+          node = Node(tempList.pop())
+          if self.head is not None:
+              node.set_next(self.head)
+          #print(self.head)
+          #print(self.head.value)
+          self.head = node
+          #print(self.head.value)
+
+newList = LinkedList()
+newList.add_to_head(1)
+newList.add_to_head(2)
+newList.add_to_head(3)
+newList.add_to_head(4)
+newList.add_to_head(5)
+print(newList.head.value)
+#print(newList.contains(1))
+#print(newList.head.get_next().value)
+newList.reverse_list()
+print(newList.head.value)
+print(newList.head.get_next().value)
+print(newList.head.get_next().get_next().value)
