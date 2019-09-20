8e4e6e93c1545b9c350fcab9cf408a13aba760dc
Doug <me@douglasbowen.com>
2019-09-20 12:51:35 -0400

    working ring buffer (passes test)

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -16,18 +16,33 @@ class RingBuffer:
    if None in self.storage:
        for i in range(len(self.storage)):
            if self.storage[i] is None:
+               if i == 0:
+                   self.current = item
+                   self.storage[i] = item
+                   return 0
+               else:
+                   self.storage[i] = item
+                   return 0
+   else:
+       for i in range(len(self.storage)):
+           print(self.storage[i])
+           print(self.current)
+           if self.storage[i] == self.current:
                self.storage[i] = item
+               if i+1 == len(self.storage):
+                   self.current = self.storage[0]
+               else:
+                   self.current = self.storage[i+1]
                return 0
-               
   def get(self):
-    print(self.storage)
+      # apparently we can't display lists with none in the ring buffer so we have to write some code to chop off the none when displaying it if there's none in the list.
+      # essentially
+      fakeList = []
+      if None in self.storage:
+          for i in range(len(self.storage)):
+              if self.storage[i] is not None:
+                  fakeList.append(self.storage[i])
+          return fakeList
+      else:
+          return self.storage
 
-rbuffer = RingBuffer(3)
-rbuffer.get()
-rbuffer.append('a')
-rbuffer.get()
-rbuffer.append('b')
-rbuffer.get()
-rbuffer.append('c')
-rbuffer.get()
-rbuffer.append('d')
